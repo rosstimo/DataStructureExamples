31e661248bdfc0f178862a7590b2f6685495f8d2
rosstimo <rosstimo@isu.edu>
2021-09-29 08:46:01 -0600

    add special roll a die function an test

--- DataStructureExamples/DataStructures.vb
+++ DataStructureExamples/DataStructures.vb
@@ -35,27 +35,28 @@
     End Sub
 
     Sub TestRandom()
-        Dim biggestNumber As Integer = 5
+        Dim biggestNumber As Integer = 6
         Dim RandomNumbers(biggestNumber) As Integer
         Dim pad As Integer = 6
         Dim headerLength As Integer = (biggestNumber + 1) * (pad + 2)
-
+        Dim rolls(12) As Integer
 
         'get random numbers
         For i = 0 To 100000
-            RandomNumbers(RandomNumberZeroTo(biggestNumber)) += 1
+            'RandomNumbers(RandomNumberZeroTo(biggestNumber) + 1) += 1
+            rolls(RollADie() + RollADie()) += 1
         Next
-        'write header
-        For i = LBound(RandomNumbers) To UBound(RandomNumbers)
-            Console.Write(CStr(i).PadLeft(pad) & " |")
-        Next
-        Console.WriteLine()
-        Console.WriteLine(StrDup(headerLength, "-"))
-        'write values
-        For i = LBound(RandomNumbers) To UBound(RandomNumbers)
-            Console.Write(CStr(RandomNumbers(i)).PadLeft(pad) & " |")
-        Next
-
+        ''write header
+        'For i = LBound(RandomNumbers) To UBound(RandomNumbers)
+        '    Console.Write(CStr(i).PadLeft(pad) & " |")
+        'Next
+        'Console.WriteLine()
+        'Console.WriteLine(StrDup(headerLength, "-"))
+        ''write values
+        'For i = LBound(RandomNumbers) To UBound(RandomNumbers)
+        '    Console.Write(CStr(RandomNumbers(i)).PadLeft(pad) & " |")
+        'Next
+        Console.ReadLine()
     End Sub
 
     Function RandomNumberZeroTo(max As Integer) As Integer
@@ -63,5 +64,12 @@
         randomNumber = CInt(System.Math.Floor(Rnd() * (max + 1)))
         Return randomNumber
     End Function
+
+    Function RollADie() As Integer
+        Dim roll As Integer
+        roll = CInt(System.Math.Floor(Rnd() * (6))) + 1
+        Return roll
+    End Function
+
 
 End Module
